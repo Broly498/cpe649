5d6c9c202d15529d7ed1dddfef18f9312f27940d
emmanuel <er0010@uah.edu>
2021-10-19 17:11:55 -0500

    Adding power measurement output file pipe.

--- runImgGenAndFtpWithPiHat.sh
+++ runImgGenAndFtpWithPiHat.sh
@@ -6,7 +6,9 @@ set -e
 IMAGE_GENERATION_DIRECTORY=examples
 POWER_MEASUREMENT_DIRECTORY=PiHat/build
 
+mkdir -p $POWER_MEASUREMENT_RESULTS_DIRECTORY
+
 taskset 0x1 python3 $IMAGE_GENERATION_DIRECTORY/networked-encrypt-image-data.py & sleep 10
 taskset 0x2 python3 $IMAGE_GENERATION_DIRECTORY/networked-compress-image-data.py & sleep 10
 taskset 0x4 python3 $IMAGE_GENERATION_DIRECTORY/networked-cloud-discrimination.py & sleep 10
-sudo taskset 0x8 ./$POWER_MEASUREMENT_DIRECTORY/profiler
+sudo taskset 0x8 ./$POWER_MEASUREMENT_DIRECTORY/profiler > piHatPowerMeasurementResults.txt
