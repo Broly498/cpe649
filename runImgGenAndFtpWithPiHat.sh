f07c42cd3f051e3f2c6b6b2aca1c3ee5bdfacbb0
emmanuel <er0010@uah.edu>
2021-10-19 17:41:15 -0500

    Correcting output folder.

--- runImgGenAndFtpWithPiHat.sh
+++ runImgGenAndFtpWithPiHat.sh
@@ -12,4 +12,4 @@ mkdir -p $PI_HAT_POWER_MEASUREMENTS_OUTPUT_DIRECTORY
 taskset 0x1 python3 $IMAGE_GENERATION_DIRECTORY/networked-encrypt-image-data.py & sleep 10
 taskset 0x2 python3 $IMAGE_GENERATION_DIRECTORY/networked-compress-image-data.py & sleep 10
 taskset 0x4 python3 $IMAGE_GENERATION_DIRECTORY/networked-cloud-discrimination.py & sleep 10
-sudo taskset 0x8 ./$POWER_MEASUREMENT_DIRECTORY/profiler -p -t 10 > piHatResults.csv
+sudo taskset 0x8 ./$POWER_MEASUREMENT_DIRECTORY/profiler -p -t 10 > $PI_HAT_POWER_MEASUREMENTS_OUTPUT_DIRECTORY/piHatResults.csv
