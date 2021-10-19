2eb8078363b55123c2934dcd7d0cd4d212a51e32
emmanuel <er0010@uah.edu>
2021-10-19 17:32:03 -0500

    Pipe power measurements to file and change update rate to 10 Hz.

--- runImgGenAndFtpWithPiHat.sh
+++ runImgGenAndFtpWithPiHat.sh
@@ -5,10 +5,11 @@ set -e
 
 IMAGE_GENERATION_DIRECTORY=examples
 POWER_MEASUREMENT_DIRECTORY=PiHat/build
+PI_HAT_POWER_MEASUREMENTS_OUTPUT_DIRECTORY=powerMeasurementResults
 
-mkdir -p $POWER_MEASUREMENT_RESULTS_DIRECTORY
+mkdir -p $PI_HAT_POWER_MEASUREMENTS_OUTPUT_DIRECTORY
 
 taskset 0x1 python3 $IMAGE_GENERATION_DIRECTORY/networked-encrypt-image-data.py & sleep 10
 taskset 0x2 python3 $IMAGE_GENERATION_DIRECTORY/networked-compress-image-data.py & sleep 10
 taskset 0x4 python3 $IMAGE_GENERATION_DIRECTORY/networked-cloud-discrimination.py & sleep 10
-sudo taskset 0x8 ./$POWER_MEASUREMENT_DIRECTORY/profiler > piHatPowerMeasurementResults.txt
+sudo taskset 0x8 ./$POWER_MEASUREMENT_DIRECTORY/profiler -p -t 10 > piHatResults.csv
