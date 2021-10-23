c6bf34b9ab3a98c796f96bdea4d90c4e66a4a126
emmanuel <er0010@uah.edu>
2021-10-23 01:06:37 -0500

    - Prepending timestamp to output power measurement .csv file.

--- runImgGenAndFtpWithPiHat.sh
+++ runImgGenAndFtpWithPiHat.sh
@@ -13,7 +13,9 @@ PI_HAT_POWER_MEASUREMENTS_OUTPUT_DIRECTORY=powerMeasurementResults
 
 mkdir -p $PI_HAT_POWER_MEASUREMENTS_OUTPUT_DIRECTORY
 
+current_time=$(date "+%Y_%m_%d-%H_%M_%S")
+
 taskset 0x1 python3 $IMAGE_GENERATION_DIRECTORY/networked-encrypt-image-data.py $1 $2  & sleep 10
 taskset 0x2 python3 $IMAGE_GENERATION_DIRECTORY/networked-compress-image-data.py & sleep 10
 taskset 0x4 python3 $IMAGE_GENERATION_DIRECTORY/networked-cloud-discrimination.py & sleep 10
-sudo taskset 0x8 ./$POWER_MEASUREMENT_DIRECTORY/profiler -p -t 10 > $PI_HAT_POWER_MEASUREMENTS_OUTPUT_DIRECTORY/piHatResults.csv
+sudo taskset 0x8 ./$POWER_MEASUREMENT_DIRECTORY/profiler -p -t 10 > $PI_HAT_POWER_MEASUREMENTS_OUTPUT_DIRECTORY/${current_time}_piHatResuls.csv
