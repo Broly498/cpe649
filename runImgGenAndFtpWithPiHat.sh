60221681a5819f1bbb4361a9211ce95b7191b75e
wes4 <cribbs.rw@gmail.com>
2021-10-28 19:08:36 -0500

    Adding retry to networking and including more command line parameters. (#19)
    * Adding retry to networking and including more command line parameters.
    
    * Addressing Pull Request Comments
    
    * Modifying command line parameters and to better match startup script.
    
    * Modifying startup script to use assigned variables instead of directly referencing command line parameters.
    
    * Minor changes to startup script that I forgot to save last time.
    
    * Converting ftp server port command line parameter from string to int.
    
    * Adding file path to encryption. Adding maximum number of timeouts to connections in discrimination and compression.
    
    * Setting default encryption bind address to 0.0.0.0

--- runImgGenAndFtpWithPiHat.sh
+++ runImgGenAndFtpWithPiHat.sh
@@ -8,9 +8,11 @@ set -e
 # argv2 - FTP Server Port
 # argv3 - Compression Data Client IP Address
 # argv4 - Image Data Client IP Address
+# argv5 - Encryption Data Client IP Address
 
 COMPRESSION_DATA_CLIENT_IP_ADDRESS=$3
 IMAGE_DATA_CLIENT_IP_ADDRESS=$4
+ENCRYPTION_DATA_CLIENT_IP_ADDRESS=$5
 
 if [ -z "$COMPRESSION_DATA_CLIENT_IP_ADDRESS" ]
   then
@@ -24,6 +26,12 @@ if [ -z "$IMAGE_DATA_CLIENT_IP_ADDRESS" ]
     IMAGE_DATA_CLIENT_IP_ADDRESS=localhost
 fi
 
+if [ -z "$ENCRYPTION_DATA_CLIENT_IP_ADDRESS" ]
+  then
+    echo "Encryption Data Client IP address was not supplied, defaulting to localhost..."
+    ENCRYPTION_DATA_CLIENT_IP_ADDRESS=localhost
+fi
+
 IMAGE_GENERATION_DIRECTORY=examples
 POWER_MEASUREMENT_DIRECTORY=PiHat/build
 PI_HAT_POWER_MEASUREMENTS_OUTPUT_DIRECTORY=powerMeasurementResults
@@ -32,7 +40,7 @@ mkdir -p $PI_HAT_POWER_MEASUREMENTS_OUTPUT_DIRECTORY
 
 current_time=$(date "+%Y_%m_%d-%H_%M_%S")
 
-taskset 0x1 python3 $IMAGE_GENERATION_DIRECTORY/networked-encrypt-image-data.py $1 $2  & sleep 10
-taskset 0x2 python3 $IMAGE_GENERATION_DIRECTORY/networked-compress-image-data.py $3 & sleep 10
-taskset 0x4 python3 $IMAGE_GENERATION_DIRECTORY/networked-cloud-discrimination.py $4 & sleep 10
+taskset 0x1 python3 $IMAGE_GENERATION_DIRECTORY/networked-encrypt-image-data.py $ENCRYPTION_DATA_CLIENT_IP_ADDRESS $1 $2  & sleep 10
+taskset 0x2 python3 $IMAGE_GENERATION_DIRECTORY/networked-compress-image-data.py $COMPRESSION_DATA_CLIENT_IP_ADDRESS & sleep 10
+taskset 0x4 python3 $IMAGE_GENERATION_DIRECTORY/networked-cloud-discrimination.py $IMAGE_DATA_CLIENT_IP_ADDRESS & sleep 10
 sudo taskset 0x8 ./$POWER_MEASUREMENT_DIRECTORY/profiler -p -t 10 > $PI_HAT_POWER_MEASUREMENTS_OUTPUT_DIRECTORY/${current_time}_piHatResuls.csv
