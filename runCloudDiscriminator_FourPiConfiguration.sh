3c64e42875824a9ac77b71212ee1ce70114d24bd
Emmanuel Morales Ramirez <er0010@uah.edu>
2021-11-02 00:06:05 +0000

    - Updating processor idle times to 300 seconds (5 minutes).

--- runCloudDiscriminator_FourPiConfiguration.sh
+++ runCloudDiscriminator_FourPiConfiguration.sh
@@ -22,7 +22,7 @@ mkdir -p $PI_HAT_POWER_MEASUREMENTS_OUTPUT_DIRECTORY
 
 current_time=$(date "+%Y_%m_%d-%H_%M_%S")
 
-PROFILER_IDLE_TIME_s=1
+PROFILER_IDLE_TIME_s=300
 
 sudo taskset 0x8 ./$POWER_MEASUREMENT_DIRECTORY/profiler -p -t 10 > $PI_HAT_POWER_MEASUREMENTS_OUTPUT_DIRECTORY/${current_time}_CloudDiscriminatorPiHatResuls.csv & sleep $PROFILER_IDLE_TIME_s
 taskset 0x4 python3 $IMAGE_GENERATION_DIRECTORY/networked-cloud-discrimination.py $IMAGE_DATA_CLIENT_IP_ADDRESS &
